30e5ad01ff1e8e3551d90442c04a78ebd7fe26e7
Adrián Ponce Cano <adrianponcecano@gmail.com>
2019-06-05 00:53:49 +0200

    navbar

---
+++ yogurtshop/src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class = 'index'>
+        <v-container class="my-5">
+        <h1> Homepage </h1>
+        </v-container>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
